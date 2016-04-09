3d8108b0d085cee5ff2c91d0c36a7ba4e83674ec
Rick <rmangilit@orchestronix.com>
2016-04-09 15:32:22 +0800

    module 4 solution

--- module-4-solution/js/script.js
+++ module-4-solution/js/script.js
@@ -108,7 +108,7 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      var chosenCategoryShortName = chooseRandomCategory(categories);
+      var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
       
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -122,7 +122,9 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       // 
-      var homeHtmlToInsertIntoMainPage = $dc.loadMenuItems(chooseRandomCategory(categories).short_name);
+      var homeHtml = $dc.loadMenuItems(chosenCategoryShortName);
+     insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
+     var homeHtmlToInsertIntoMainPage = homeHtml;
       
       
 
